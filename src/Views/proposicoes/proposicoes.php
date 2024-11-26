7315ab1344f4607772a15f22e2018f028bd6c824
Jairo Santos <jairojeffersont@gmail.com>
2024-11-26 17:08:17 -0300

    v1

--- src/Views/proposicoes/proposicoes.php
+++ src/Views/proposicoes/proposicoes.php
@@ -12,7 +12,7 @@ $proposicaoController = new ProposicaoController();
 $itens = isset($_GET['itens']) ? $_GET['itens'] : 10;
 $pagina = isset($_GET['pagina']) ? $_GET['pagina'] : 1;
 $ordenarPor = isset($_GET['ordenarPor']) ? $_GET['ordenarPor'] : 'proposicao_id';
-$ordem = isset($_GET['ordem']) ? $_GET['ordem'] : 'ASC';
+$ordem = isset($_GET['ordem']) ? $_GET['ordem'] : 'desc';
 $termo = isset($_GET['termo']) ? $_GET['termo'] : '';
 $ano = isset($_GET['ano']) ? $_GET['ano'] : date('Y');
 $tipo = isset($_GET['tipo']) ? $_GET['tipo'] : 'PL';
@@ -28,7 +28,7 @@ $arquivada = isset($_GET['arquivada']) ? filter_var($_GET['arquivada'], FILTER_V
             <div class="card mb-2 ">
                 <div class="card-body p-1">
                     <a class="btn btn-primary btn-sm custom-nav card-description" href="?secao=home" role="button"><i class="bi bi-house-door-fill"></i> Início</a>
-                    <a class="btn btn-success btn-sm custom-nav card-description" href="?secao=imprimir-proposicoes&pagina=1&ano=<?php echo $ano ?>&tipo=<?php echo $tipo ?>&arquivada=<?php echo $arquivada ?>" target="_blank" role="button"><i class="bi bi-printer-fill"></i> Imprimir</a>
+                    <a class="btn btn-success btn-sm custom-nav card-description" href="?secao=imprimir-proposicoes&pagina=1&ano=<?php echo $ano ?>&tipo=<?php echo $tipo ?>&arquivada=<?php echo $arquivada ?>&termo=<?php echo $termo?>" target="_blank" role="button"><i class="bi bi-printer-fill"></i> Imprimir</a>
                 </div>
             </div>
             <div class="card mb-2 card-description ">
