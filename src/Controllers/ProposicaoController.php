ad881d12e643d5530c1b9c12ecbfdb429182eba7
Jairo Santos <jairojeffersont@gmail.com>
2024-11-26 18:39:56 -0300

    v1

--- src/Controllers/ProposicaoController.php
+++ src/Controllers/ProposicaoController.php
@@ -128,47 +128,13 @@ class ProposicaoController {
 
     public function buscarUltimaProposicao($id) {
         $url = 'https://dadosabertos.camara.leg.br/api/v2/proposicoes/' . $id;
-        $primeiroResultado = null;
-        $ultimoResultado = null;
-        $segundaIteracao = false; // Para controlar a segunda iteração
-    
-        do {
+        $resposta = $this->getjson->getJson($url);
+
+        while (isset($resposta['dados']['uriPropPrincipal']) && $resposta['dados']['uriPropPrincipal'] !== null) {
+            $url = $resposta['dados']['uriPropPrincipal'];
             $resposta = $this->getjson->getJson($url);
-    
-            if (isset($resposta['dados'])) {
-                $dados = $resposta['dados'];
-    
-                if ($segundaIteracao && !$primeiroResultado) {
-                    // Armazena o primeiro resultado da segunda iteração
-                    $primeiroResultado = $dados;
-                }
-    
-                $ultimoResultado = $dados; // Atualiza o último resultado
-    
-                if (!empty($dados['uriPropPrincipal'])) {
-                    $url = $dados['uriPropPrincipal'];
-                } else {
-                    $url = null;
-                }
-    
-                $segundaIteracao = true; // Marca que a segunda iteração já começou
-            } else {
-                break;
-            }
-        } while ($url);
-    
-        // Verifica se houve algum erro (se não houve iteração)
-        if ($ultimoResultado === $primeiroResultado) {
-            return ['status' => 'empty', 'dados' => []];
         }
-    
-        // Retorna o primeiro e o último resultado conforme solicitado
-        return [
-            'status' => 'success',
-            'primeiroResultado' => $primeiroResultado,
-            'ultimoResultado' => $ultimoResultado
-        ];
+
+        return ['status' => 'success', 'dados' => $resposta['dados']];
     }
-    
-    
 }
--- src/Views/proposicoes/proposicao.php
+++ src/Views/proposicoes/proposicao.php
@@ -31,8 +31,7 @@ if ($buscaProposicao['status'] != 'success') {
                 <div class="card-body p-1">
                     <a class="btn btn-primary btn-sm custom-nav card-description" href="?secao=home" role="button"><i class="bi bi-house-door-fill"></i> Início</a>
                     <a class="btn btn-success btn-sm custom-nav card-description" href="?secao=proposicoes" role="button"><i class="bi bi-arrow-left"></i> Voltar</a>
-
-                    <a class="btn btn-secondary btn-sm custom-nav card-description" href="?secao=imprimir-proposicoes&pagina=1&ano=<?php echo $ano ?>&tipo=<?php echo $tipo ?>&arquivada=<?php echo $arquivada ?>&termo=<?php echo $termo ?>" target="_blank" role="button"><i class="bi bi-printer-fill"></i> Imprimir</a>
+                    <a class="btn btn-secondary btn-sm custom-nav card-description" href="?secao=imprimir-ficha-proposicoes&id=<?php echo $id ?>" target="_blank" role="button"><i class="bi bi-printer-fill"></i> Imprimir</a>
                 </div>
             </div>
             <div class="card mb-2 card-description ">
@@ -44,44 +43,38 @@ if ($buscaProposicao['status'] != 'success') {
 
             <div class="card mb-2 card-description ">
                 <div class="card-header bg-primary text-white px-2 py-1">Detalhes</div>
-                <div class="card-body p-2">
-                    <h5 class="card-title mb-0"><?php echo $buscaProposicao['dados'][0]['proposicao_titulo'] ?>
+                <div class="card-body p-3">
+                    <h5 class="card-title mb-0"><?php echo $buscaProposicao['dados'][0]['proposicao_titulo'] ?><?php echo ($buscaProposicao['dados'][0]['proposicao_arquivada']) ? ' | <small><i class="bi bi-info-circle-fill"></i> Arquivada</small>' : ''  ?>
                         <hr class="mt-2">
                     </h5>
                     <p class="card-text mb-2" style="font-size:1em"><?php echo $buscaProposicao['dados'][0]['proposicao_ementa'] ?>
                         <hr>
                     </p>
 
-                    <p class="card-text mb-2" style="font-size:1em"><i class="bi bi-calendar3"></i> Data de apresentação: <?php echo date('d/m', strtotime($buscaProposicao['dados'][0]['proposicao_apresentacao'])) ?></p>
-
+                    <p class="card-text mb-0" style="font-size:1em"><i class="bi bi-calendar3"></i> Data de apresentação: <?php echo date('d/m', strtotime($buscaProposicao['dados'][0]['proposicao_apresentacao'])) ?></p>
+                    <p class="card-text mb-2" style="font-size:1em"><a href="https://www.camara.leg.br/proposicoesWeb/fichadetramitacao/?idProposicao=<?php echo $buscaProposicao['dados'][0]['proposicao_id'] ?>" target="_blank">Ver página da Câmara</a></p>
 
                     <?php
+
                     if (count($buscarAutores['dados']) > 1) {
                         echo '<hr>';
                         foreach ($buscarAutores['dados'] as $autor) {
                             echo '<p class="card-text mb-0" style="font-size:1em"><i class="bi bi-person-fill"></i> ' . $autor['proposicao_autor_nome'] . ' ' . $autor['proposicao_autor_partido'] . '/' . $autor['proposicao_autor_estado'] . '</p>';
                         }
                     }
-                    ?>
 
+                    ?>
 
                     <?php
 
                     if ($buscaPrincipal['status'] == 'success') {
-
-                        //print_r($buscaPrincipal);
-
-                        echo '<hr><p class="card-text mb-1" style="font-size:1em"><i class="bi bi-arrow-right-short"></i> Proposição ao qual foi apensada: </p>';
-                        echo '<p class="card-text mb-0 ms-4" style="font-size:1em"><i class="bi bi-dot"></i> <b><a href="https://www.camara.leg.br/proposicoesWeb/fichadetramitacao?idProposicao=' . $buscaPrincipal['primeiroResultado']['id'] . '" target="_blank">' . $buscaPrincipal['primeiroResultado']['siglaTipo'] . ' ' . $buscaPrincipal['primeiroResultado']['numero'] . '/' . $buscaPrincipal['primeiroResultado']['ano'] . '</a></b></p>';
-                        echo '<p class="card-text mb-2 ms-4"><i class="bi bi-dot"></i> <a href="' . $buscaPrincipal['primeiroResultado']['urlInteiroTeor'] . '" target="_blank">Inteiro teor <i class="bi bi-file-earmark-text"></i></a> </p>';
-
-                        echo '<hr><p class="card-text mb-1" style="font-size:1em"><i class="bi bi-arrow-right-short"></i> Proposição principal: </p>';
-                        echo '<p class="card-text mb-0 ms-4" style="font-size:1em"><i class="bi bi-dot"></i> <b><a href="https://www.camara.leg.br/proposicoesWeb/fichadetramitacao?idProposicao=' . $buscaPrincipal['ultimoResultado']['id'] . '" target="_blank">' . $buscaPrincipal['ultimoResultado']['siglaTipo'] . ' ' . $buscaPrincipal['ultimoResultado']['numero'] . '/' . $buscaPrincipal['ultimoResultado']['ano'] . '</a></b></p>';
-                        echo '<p class="card-text mb-2 ms-4"><i class="bi bi-dot"></i> <a href="' . $buscaPrincipal['ultimoResultado']['urlInteiroTeor'] . '" target="_blank">Inteiro teor <i class="bi bi-file-earmark-text"></i></a> </p>';
-
+                        if ($buscaPrincipal['dados']['id'] != $id) {
+                            echo '<hr><p class="card-text mb-1" style="font-size:1em"><i class="bi bi-arrow-right-short"></i> Proposição principal: </p>';
+                            echo '<p class="card-text mb-0 ms-4" style="font-size:1em"><i class="bi bi-dot"></i> <b>' . $buscaPrincipal['dados']['siglaTipo'] . ' ' . $buscaPrincipal['dados']['numero'] . '/' . $buscaPrincipal['dados']['ano'] . '</b></p>';
+                            echo '<p class="card-text mb-2 ms-4"><i class="bi bi-dot"></i> <a href="https://www.camara.leg.br/proposicoesWeb/fichadetramitacao?idProposicao=' . $buscaPrincipal['dados']['id'] . '" target="_blank">Ver página da Câmara</a> </p>';
+                        }
                     }
                     ?>
-
 
                 </div>
             </div>
