609f59f085aa674604b0b1c886331f2a8139dcb6
Jairo Santos <jairojeffersont@gmail.com>
2024-11-26 14:04:07 -0300

    v1

--- src/Views/home/home.php
+++ src/Views/home/home.php
@@ -1,9 +1,9 @@
 <?php include '../src/Views/includes/verificaLogado.php';?>
 
 <div class="d-flex" id="wrapper">
-    <?php include '../src/views/includes/sider_bar.php'; ?>
+    <?php include '../src/Views/includes/sider_bar.php'; ?>
     <div id="page-content-wrapper">
-        <?php include '../src/views/includes/top_menu.php'; ?>
+        <?php include '../src/Views/includes/top_menu.php'; ?>
         <div class="container-fluid p-2">
 
         </div>
