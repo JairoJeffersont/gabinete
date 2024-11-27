d36d4bf937202be59e85dab8753d8dae0afba409
Jairo Santos <jairojeffersont@gmail.com>
2024-11-27 16:02:09 -0300

    v1

--- src/Views/cadastro/cadastro.php
+++ src/Views/cadastro/cadastro.php
@@ -1,3 +1,13 @@
+<?php
+
+
+require_once '../autoloader.php';
+
+use GabineteDigital\Controllers\UsuarioController;
+
+$usuarioController = new UsuarioController();
+
+?>
 <link href="css/cadastro.css" rel="stylesheet">
 <div class="d-flex justify-content-center align-items-center vh-100">
     <div class="centralizada text-center">
@@ -5,7 +15,37 @@
         <img src="public/img/logo_white.png" alt="" class="img_logo" />
         <h2 class="login_title">Gabinete Digital</h2>
         <h6 class="host"><?php echo $_SERVER['HTTP_HOST'] ?></h6>
+        <?php
+        if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['btn_salvar'])) {
+            if ($_POST['usuario_senha'] !== $_POST['usuario_senha2']) {
+                echo '<div class="alert alert-info px-2 py-1 mb-2 custom-alert" data-timeout="3" role="alert">As senha não conferem</div>';
+            } elseif (strlen($_POST['usuario_senha']) < 6) {
+                echo '<div class="alert alert-danger px-2 py-1 mb-2 custom-alert" data-timeout="3" role="alert">A senha tem menos de 6 caracteres</div>';
+            } else {
+                $usuario = [
+                    'usuario_nome' => htmlspecialchars($_POST['usuario_nome'], ENT_QUOTES, 'UTF-8'),
+                    'usuario_email' => htmlspecialchars($_POST['usuario_email'], ENT_QUOTES, 'UTF-8'),
+                    'usuario_telefone' => htmlspecialchars($_POST['usuario_telefone'], ENT_QUOTES, 'UTF-8'),
+                    'usuario_aniversario' => htmlspecialchars($_POST['usuario_aniversario'], ENT_QUOTES, 'UTF-8'),
+                    'usuario_ativo' => 0,
+                    'usuario_nivel' => 2,
+                    'usuario_senha' => htmlspecialchars($_POST['usuario_senha'], ENT_QUOTES, 'UTF-8')
+                ];
 
+
+                $result = $usuarioController->criarUsuario($usuario);
+
+                if ($result['status'] == 'success') {
+                    echo '<div class="alert alert-success px-2 py-1 mb-2 custom-alert" data-timeout="3" role="alert">' . $result['message'] . '</div>';
+                } else if ($result['status'] == 'duplicated' || $result['status'] == 'bad_request' || $result['status'] == 'invalid_email') {
+                    echo '<div class="alert alert-info px-2 py-1 mb-2 custom-alert" data-timeout="3" role="alert">' . $result['message'] . '</div>';
+                } else if ($result['status'] == 'error' || $result['status'] == 'forbidden') {
+                    echo '<div class="alert alert-danger px-2 py-1 mb-2 custom-alert" data-timeout="0" role="alert">' . $result['message'] . '</div>';
+                }
+            }
+        }
+
+        ?>
         <form class="row g-2 form_custom" id="form_novo" method="POST" enctype="multipart/form-data">
             <div class="col-md-12 col-12">
                 <input type="text" class="form-control form-control-sm" name="usuario_nome" placeholder="Nome" required>
@@ -14,7 +54,7 @@
                 <input type="email" class="form-control form-control-sm" name="usuario_email" placeholder="Email" required>
             </div>
             <div class="col-md-6 col-6">
-                <input type="text" class="form-control form-control-sm" name="usuario_telefone" placeholder="Celular (com DDD)" data-mask="(00) 00000-0000" maxlength="11" required>
+                <input type="text" class="form-control form-control-sm" name="usuario_telefone" placeholder="Celular (com DDD)" data-mask="(00) 00000-0000" maxlength="15" required>
             </div>
             <div class="col-md-6 col-6">
                 <input type="text" class="form-control form-control-sm" name="usuario_aniversario" placeholder="dd/mm" data-mask="00/00" required>
@@ -26,7 +66,7 @@
                 <input type="password" class="form-control form-control-sm" name="usuario_senha2" placeholder="Confirme a senha" required>
             </div>
             <div class="d-flex justify-content-between align-items-center">
-                <button type="submit" name="btn_login" class="btn btn-primary">Salvar</button>
+                <button type="submit" name="btn_salvar" class="btn btn-primary">Salvar</button>
                 <a type="button" href="?secao=login" class="btn btn-secondary">Voltar</a>
 
             </div>
